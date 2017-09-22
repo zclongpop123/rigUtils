ecec1d3f4e079970a72e675f9b0d161846e23793
Changlong.Zang <zclongpop123@163.com>
2017-09-22 10:52:57 +0800

    add namespace tool...

---
+++ namespace.py
@@ -0,0 +1,16 @@
+#========================================
+#    author: Changlong.Zang
+#      mail: zclongpop123@163.com
+#      time: Fri Sep 22 10:50:56 2017
+#========================================
+import maya.cmds as mc
+#--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+
+def clear_all_namespace():
+    '''
+    '''
+    namespaces = mc.namespaceInfo(listOnlyNamespaces=True, recurse=True)
+    namespaces.remove('UI')
+    namespaces.remove('shared')
+    while namespaces:
+        mc.namespace(rm=namespaces.pop(0), mnr=True)
